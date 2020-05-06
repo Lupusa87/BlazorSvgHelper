413c95e0f603e45f3e9fb57264a76285688421d7
Lupusa87 <VakhtangiAbashidze@gmail.com>
2020-05-06 00:12:49 -0400

    5/5/2020

---
+++ BlazorSvgHelper/Classes/SubClasses/linearGradient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorSvgHelper.Classes.SubClasses
+{
+    public class linearGradient
+    {
+        public string id { get; set; } = null;
+        public string x1 { get; set; } = null;
+        public string x2 { get; set; } = null;
+        public string y1 { get; set; } = null;
+        public string y2 { get; set; } = null;
+        public ICollection<object> Children { get; set; } = new List<object>();
+    }
+}
